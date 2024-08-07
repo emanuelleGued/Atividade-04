0cb7cdecace3eb5e4a479b516728598755bdb887
Ester Trevisan <ester.trevisan@academico.ifpb.edu.br>
2024-08-07 11:42:08 -0300

    :bug: fix: corrigindo erro no slot da intent GetDetailsOfPizza

--- server/lambda/controllers/get-menu.js
+++ server/lambda/controllers/get-menu.js
@@ -1,7 +1,64 @@
 import { handleElicitSlotResponse, handleCloseResponse } from '../utils/response-builder.js';
 
 export const handleGetMenuIntent = async (event) => {
-  const responseMessage = `Estes são nossos itens do cardápio. Qual sabor você gostaria de saber mais?`;
+  const menu = `
+  Aqui está o nosso cardápio de pizzas:
 
-  return handleCloseResponse(event, 'Fulfilled', responseMessage);
+  Margherita:
+  - Pequena: R$ 20
+  - Média: R$ 30
+  - Grande: R$ 40
+
+  Pepperoni:
+  - Pequena: R$ 22
+  - Média: R$ 32
+  - Grande: R$ 42
+
+  Calabresa:
+  - Pequena: R$ 24
+  - Média: R$ 34
+  - Grande: R$ 44
+
+  Frango com Catupiry:
+  - Pequena: R$ 26
+  - Média: R$ 36
+  - Grande: R$ 46
+
+  Churrasco:
+  - Pequena: R$ 28
+  - Média: R$ 38
+  - Grande: R$ 48
+
+  Portuguesa:
+  - Pequena: R$ 30
+  - Média: R$ 40
+  - Grande: R$ 50
+
+  Marguerita:
+  - Pequena: R$ 20
+  - Média: R$ 30
+  - Grande: R$ 40
+
+  Napolitana:
+  - Pequena: R$ 22
+  - Média: R$ 32
+  - Grande: R$ 42
+
+  Toscana:
+  - Pequena: R$ 24
+  - Média: R$ 34
+  - Grande: R$ 44
+
+  Vegetariana:
+  - Pequena: R$ 26
+  - Média: R$ 36
+  - Grande: R$ 46
+
+  Quatro Queijos:
+  - Pequena: R$ 30
+  - Média: R$ 36
+  - Grande: R$ 40
+  `;
+
+  return handleCloseResponse(event, 'Fulfilled', menu);
 };
